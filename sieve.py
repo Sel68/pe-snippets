d7a043acdfdcc8fc45be2229ec8b3bef879c371b
sel68 <saadhasanellahie@gmail.com>
2025-09-03 10:42:40 +0500

    sieve with bytearray

--- sieve.py
+++ sieve.py
@@ -1,11 +1,9 @@
-import numpy as np
-
 def sieve(hi=int(1e6)):
     if hi < 2:
-        return np.zeros(hi, dtype = bool)
-    is_prime = np.ones(hi, dtype=bool)   
-    is_prime[0] = is_prime[1] = 0          
+        return [False]*hi
+    is_prime = bytearray(b"\x01")*hi
+    is_prime[0] = is_prime[1] = 0
     for p in range(2, int(hi**0.5) + 1):
         if is_prime[p]:
-            is_prime[p*p::p] = False
-    return is_prime
+            is_prime[p*p:hi:p] = b"\x00"*len(is_prime[p*p:hi:p])
+    return [bool(x) for x in is_prime]
