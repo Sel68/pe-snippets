87805751f828458af309a1e233dd9b8c15295f6a
sel68 <saadhasanellahie@gmail.com>
2026-01-15 17:15:13 +0500

    prime test

---
+++ miller_rabbin.py
@@ -0,0 +1,25 @@
+import random
+
+#return true for prime
+def miller_rabin(n, k=10):
+    if n < 2: return False
+    if n % 2 == 0: return n == 2
+    
+    d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+    
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(s - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
